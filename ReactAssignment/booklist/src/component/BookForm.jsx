e2bac8d9f2f9b08a5bd21dcac3675f0e7e27128f
kanika kala <kala.kanika1998@gmail.com>
2023-10-08 23:10:17 +0530

    assigment-03

--- ReactAssignment/booklist/src/component/BookForm.jsx
+++ ReactAssignment/booklist/src/component/BookForm.jsx
@@ -1,29 +1,48 @@
 import React, { useState } from 'react'
 import { BookDetail } from './BookDetail';
+import {BookList } from './BookList';
 function BookForm(){
-    const[book,setBook]=useState()
+
+
+    
+    const[book,setBook]=useState([])
     const[submit,setSubmit]=useState(false);
     const handleSubmit=(evt)=>{
-        const book={
-            title:evt.title,
-            author:evt.author,
-            year:evt.year
-        }
-        setBook(book)
-        console.log(book)
-        setSubmit(true);
-        
+        evt.preventDefault();
+        const title = evt.target.title.value;
+        const author = evt.target.author.value;
+        const year = evt.target.year.value;
+      
+        // const book={
+        //     title:evt.target.title.value,
+        //     author:evt.target.author.value,
+        //     year:evt.target.year.value
+        // }
+        // setBook(book)
+        //console.log(book)
+        if (title.trim() !== "" && author.trim() !== "" && year.trim() !== "") {
+            const newBook = {
+              title: title,
+              author: author,
+              year: year,
+            };
+            setBook(newBook);
+          } else {
+            // Display an error message or handle the validation as needed
+            console.log("Please fill in all fields");
+          }
 
     }
 
     return(
         <>
         <form onSubmit={handleSubmit}>
-        <input placeholder='Author' type="text"/>
-        <input placeholder='Title'type="text"/>
-        <input placeholder='Year' type="number"/>
+        <input name="author" placeholder='Author' type="text"/>
+        <input  name="title" placeholder='Title'type="text"/>
+        <input name="year" placeholder='Year' type="number"/>
         <input  type="submit"/>
         </form>
+        { <BookList newBook={book} />}
         </>
     )
 
--- ReactAssignment/booklist/src/component/BookList.jsx
+++ ReactAssignment/booklist/src/component/BookList.jsx
@@ -1,24 +1,23 @@
 
 import {React,Component, useState,useEffect } from "react";
-import { Book } from './Book'
 import { BookDetail } from "./BookDetail";
 
 function BookList(props){
+
     const [books,setBooks]= useState([])
-   
-    // const books = [
-    //     { title: 'Book 1', author: 'Author 1', year: 2020 },
-    //     { title: 'Book 2', author: 'Author 2', year: 2018 },
-    //     { title: 'Book 3', author: 'Author 3', year: 2022 },
-    //     // Add more books if you'd like
-    //     // read how map function workd
-    //   ];
+    useEffect(()=>{
+        // dicuss with sir that till the time i was doing 
+        if (props.newBook.title && props.newBook.author && props.newBook.year){
+            setBooks((prevBooks) => [...prevBooks, props.newBook]);
+        }
+    },[props.newBook])
     return(
         // maps takes a callbck function which is that is extecuted for each item in aaray
          // map return the array of jsx element
         //{/* <BookDetail bookDetail={book} key={index}></BookDetail> */} 
          <>
-         {books.length===0? <h1>Sorry!!-Nothing to display-No Books in the system</h1>:
+         {console.log(books)}
+         {books.length===0 ? <h1>Sorry!!-Nothing to display-No Books in the system</h1>:
          <ol> {books.map((book,index) => (<li><BookDetail bookDetail={book}></BookDetail></li>))} </ol>
          }
          </>              
